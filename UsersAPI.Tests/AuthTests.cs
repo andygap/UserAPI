3d4e13216f7610ca18a5082a8c7546e6abd2c4e9
RaNoronha <ranlcgnn@gmail.com>
2023-06-30 18:58:51 -0300

    feat:Design DDD e criação dos testes

---
+++ UsersAPI.Tests/AuthTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace UsersAPI.Tests
+{
+    public class AuthTests
+    {
+        [Fact(Skip ="Não implementado")]
+        public void Auth_Post_Login_Returns_OK()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Auth_Post_Login_Returns_Unauthorized()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Auth_Post_Forgot_Password_Returns_OK()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Auth_Post_Forgot_Password_Returns_BadRequest()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Auth_Post_Reset_Password_Returns_OK()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Auth_Post_Reset_Password_Returns_BadRequest()
+        {
+
+        }
+    }
+}
